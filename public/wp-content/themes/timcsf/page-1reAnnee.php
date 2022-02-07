e219f371c8749c7c4eeed748121c1a061b4898e5
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-02-07 12:42:18 -0500

    commit Ajouter au menu

---
+++ public/wp-content/themes/timcsf/page-1reAnnee.php
@@ -0,0 +1,31 @@
+<?php get_header();
+
+/*Template name: 1re Année*/
+
+echo "page-1reAnnee.php";
+
+if(is_active_sidebar("gauche")){?>
+    <aside id="gauche" class="barregauche">
+        <?php get_sidebar("gauche")?>
+    </aside>
+<?php } ?>
+
+    <main class="page">
+        <?php the_post();?>
+        <div class="entetePage">
+            <h2><?php the_title()?></h2>
+        </div>
+        <p>
+            <?php the_content();?>
+        </p>
+    </main>
+
+
+<?php
+if(is_active_sidebar("droite")){?>
+    <aside id="droite" class="barredroite">
+        <?php get_sidebar("droite")?>
+    </aside>
+<?php } ?>
+
+<?php get_footer(); ?>
