3669179d9fe91c17721542f2e2ef5a5ebe5b74cc
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-02-14 17:07:52 -0500

    commit vendredi17h

--- public/wp-content/themes/timcsf/liaisons/js/menu.js
+++ public/wp-content/themes/timcsf/liaisons/js/menu.js
@@ -10,47 +10,62 @@
 //*******************
 
 var menu = {
-  //propriété conservant les libellés du bouton
   lblMenuFerme: 'Menu',
-  lblMenuOuvert: 'X',
-  //propriété conservant la référence de la balise nav
-  refMenu: $("#principal"),
+  lblMenuOuvert: 'Fermer',
+  refBouton: null,
+  refLibelle: null,
+  refMenu: null,
 
   configurerNav: function ()
   {
-    //si le javascript et activé, la classe js est placée dans le body
-    $("body").addClass("js");
+    //********** Création du bouton du menu mobile
 
-    //injection de la classe menu dans la balise nav
-    this.refMenu.attr("class","menu menu--ferme");
+    // On sélectionne le menu dans le HTML
+    this.refMenu = document.querySelector(".menu");
 
-    //injection des classes de la liste des liens
-    this.refMenu.find("ul").addClass("menu__liste");
-    this.refMenu.find("li").addClass("menu__listeItem");
-    this.refMenu.find("a").addClass("menu__lien");
+    // Création du bouton et du libellé
+    this.refBouton = document.createElement("button");
+    this.refLibelle = document.createElement("span");
 
-    //Création du bouton du menu mobile
-    this.refMenu.prepend('<button class="menu__controle"><span class="menu__libelle" >'+this.lblMenuOuvert+'</span></button>');
-    //Afficher l'état fermer du libellé
-    $(".menu__libelle").html(this.lblMenuFerme);
-    //Ajout de l'écouteur d'événement sur le bouton du menu
-    $(".menu__controle").on("click",this.ouvrirFermerNav.bind(this));
+    // On ajoute le libellé dans le bouton
+    this.refBouton.appendChild(this.refLibelle);
+
+    // On ajoute les classes au bouton et au libellé
+    this.refBouton.className = 'menu__controle';
+    this.refLibelle.className = 'menu__libelle';
+
+    // On associe le texte du libellé à l'élément html
+    this.refLibelle.innerHTML = this.lblMenuFerme;
+
+    // Ajout du bouton dans l'entête de la page html
+    this.refMenu.prepend(this.refBouton);
+
+    // Ajout de l'écouteur d'événement sur le bouton du menu
+    /*this.refBouton.addEventListener('click', function () {
+      menu.ouvrirFermerNav();
+    });*/
+    this.refBouton.addEventListener('click', this.ouvrirFermerNav.bind(this));
   },
 
   ouvrirFermerNav: function ()
   {
     // On bascule la classe de fermeture du menu
-    this.refMenu.toggleClass("menu--ferme");
+    this.refMenu.classList.toggle("menu--ferme");
 
     // On change le libellé du bouton selon l'état du menu
-    if(this.refMenu.hasClass("menu--ferme")){
-      $(".menu__libelle").html(this.lblMenuFerme);
-    }else{
-      $(".menu__libelle").html(this.lblMenuOuvert);
+    if (this.refMenu.classList.contains("menu--ferme"))
+    {
+      this.refLibelle.innerHTML = this.lblMenuFerme;
+    }
+    else
+    {
+      this.refLibelle.innerHTML = this.lblMenuOuvert;
     }
   }
 };
-//*******************
+
+
+
 // Écouteurs d'événements
 //*******************
-$(document).ready(menu.configurerNav.bind(menu));
+window.addEventListener('load', function () { menu.configurerNav(); });
