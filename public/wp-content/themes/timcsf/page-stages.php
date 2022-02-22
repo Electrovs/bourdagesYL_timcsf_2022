44bb79b9901144eae7bad69235ba75f002fb59c4
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-02-22 15:40:49 -0500

    commit mardi15h40

--- public/wp-content/themes/timcsf/page-stages.php
+++ public/wp-content/themes/timcsf/page-stages.php
@@ -2,19 +2,20 @@
 
 /*Template name: Stages*/
 
-echo "page-stages.php";
+//echo "page-stages.php";?>
 
-if(is_active_sidebar("gauche")){?>
-    <aside id="gauche" class="barregauche">
-        <?php get_sidebar("gauche")?>
-    </aside>
-<?php } ?>
+    <div class="header__bandeau horsAccueil">
+        <div class="header__bandeau__blockTitre">
+            <h1 class="header__bandeau__blockTitre__titre"><?php echo get_post(468)->titre?></h1>
+            <p class="header__bandeau__blockTitre__p"><?php echo get_post(468)->texte?></p>
+        </div>
+    </div>
 </header>
 <main class="stages">
     <div class="conteneur">
         <h1 class="stages__titre">Stages</h1>
         <p class="stages__texteIntro">
-            console.log()Le programme TIM du Cégep de Sainte-Foy offre des stages
+            Le programme TIM du Cégep de Sainte-Foy offre des stages
             en Alternance travail études (ATE) pendant l’été et un
             stage crédité en session terminale qui peut être réalisé en France.
         </p>
@@ -27,10 +28,10 @@ if(is_active_sidebar("gauche")){?>
                 </p>
                 <div class="stages__stagesInfo__stageATE__dureeDate">
                     <p class="stages__stagesInfo__stageATE__date">
-                        Débute le 24 mai 2022
+                        <span class="stages__stagesInfo__stageATE__date__span">Débute le</span> 24 mai 2022
                     </p>
                     <p class="stages__stagesInfo__stageATE__duree">
-                        Durée de 8 semaines minimum
+                        <span class="stages__stagesInfo__stageATE__duree__span">Durée</span> de 8 semaines minimum
                     </p>
                 </div>
             </div>
@@ -43,32 +44,32 @@ if(is_active_sidebar("gauche")){?>
                 </p>
                 <div class="stages__stagesInfo__stageFE__dureeDate">
                     <p class="stages__stagesInfo__stageFE__date">
-                        Débute le 21 mars 2022
+                        <span class="stages__stagesInfo__stageFE__date__span">Débute le</span> 21 mars 2022
                     </p>
                     <p class="stages__stagesInfo__stageFE__duree">
-                        Durée de 8 semaines
+                        <span class="stages__stagesInfo__stageFE__duree__span">Durée</span> de 8 semaines
                     </p>
                 </div>
             </div>
         </div>
         <div class="stages__employeurs">
             <h2 class="stages__employeurs__titre">Employeurs</h2>
-            <p class="stages__employeurs__texte">Consultez le profil des compétences des étudiants pour déterminer le stage à offrir.</p>
+            <div class="stages__employeurs__texte">
+                <p class="stages__employeurs__texte__01">Les stages rémunérés sont admissibles à des crédits d’impôt avantageux.</p>
+                <p class="stages__employeurs__texte__02">Consultez le <span class="stages__employeurs__texte__span">profil des compétences des étudiants</span> pour déterminer le stage à offrir.</p>
+            </div>
         </div>
         <div class="stages__contactezPascal">
             <div class="stages__contactezPascal__infos">
                 <p class="stages__contactezPascal__infos__texte">Contactez Pascal Larose pour en savoir plus.</p>
-                <p class="stages__contactezPascal__infos__telephone">Téléphone : (418) 659-6600 poste 6653</p>
+                <p class="stages__contactezPascal__infos__telephone">Téléphone : <span class="stages__contactezPascal__infos__telephone__span">(418) 659-6600 poste 6653</span></p>
             </div>
-            <a class="stages__contactezPascal__boutonPascal" href="#">Écris à Pascal Larrose</a>
+            <a class="stages__contactezPascal__boutonPascal boutonBleu" href="#">Écris à Pascal Larrose</a>
+        </div>
+        <div class="boutonHautPage">
+            <a class="boutonHautPage__bouton fa fa-arrow-up" href="#top"></a>
         </div>
     </div>
 </main>
-<?php
-if(is_active_sidebar("droite")){?>
-    <aside id="droite" class="barredroite">
-        <?php get_sidebar("droite")?>
-    </aside>
-<?php } ?>
 
 <?php get_footer(); ?>
