9fc2fb60f82d16fcc019aec5f378dffb2a256232
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-03-01 15:43:52 -0500

    commit mardi 15h40

--- public/wp-content/themes/timcsf/single-projets.php
+++ public/wp-content/themes/timcsf/single-projets.php
@@ -2,7 +2,69 @@
 
 /*Template name: Fiche Projets*/
 
-//echo "single-projets.php"; ?>
+//echo "single-projets.php";
+
+$idProjetVue = $post->ID;
+
+
+
+
+//Les Finissants
+//**************************************************
+
+//Requête pour obtenir les infos des finissants
+$args = array(
+    'post_type' => 'diplômés',
+    'posts_per_page' => -1,
+    'post_status' => 'publish',
+    'meta_key' => 'id',
+    'meta_value' => get_field("diplome_id"),
+
+);
+
+$the_query = new WP_Query($args);
+
+$arrFinissants=array();
+if( $the_query->have_posts() ) {
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        array_push($arrFinissants, $post);
+    }
+}
+wp_reset_postdata();
+
+//Les Cours
+//**************************************************
+
+//Requête pour obtenir les infos des finissants
+$args = array(
+    'post_type' => 'cours',
+    'posts_per_page' => -1,
+    'post_status' => 'publish',
+    'meta_key' => 'id',
+    'meta_value' => get_field("cours_id"),
+
+);
+
+$the_query = new WP_Query($args);
+
+$arrCours=array();
+if( $the_query->have_posts() ) {
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        array_push($arrCours, $post);
+    }
+}
+wp_reset_postdata();
+$sessionAnnee = "";
+if(get_field("session", $arrCours[0]->ID) == "1" || get_field("session", $arrCours[0]->ID) == "2"){
+    $sessionAnnee = "1re";
+}elseif (get_field("session", $arrCours[0]->ID) == "3" || get_field("session", $arrCours[0]->ID) == "4"){
+    $sessionAnnee = "2e";
+}else{
+    $sessionAnnee = "3e";
+}
+?>
     <div class="header__bandeau horsAccueil">
         <div class="header__bandeau__blockTitre">
             <h1 class="header__bandeau__blockTitre__titre"><?php echo get_post(468)->titre?></h1>
@@ -21,149 +83,128 @@
             <div class="ficheRealisation__infos__infoRealisation">
                 <h1 class="ficheRealisation__infos__infoRealisation__titre"><?php echo get_field("titre") ?></h1>
                 <div class="ficheRealisation__infos__infoRealisation__nomAnnee">
-<?php
-
-//Les Finissants
-//**************************************************
-
-//Requête pour obtenir les infos des finissants
-$args = array(
-    'post_type' => 'diplômés',
-    'posts_per_page' => -1,
-    'post_status' => 'publish',
-);
-
-$the_query = new WP_Query($args);
-
-$arrFinissants=array();
-if( $the_query->have_posts() ) {
-    while ( $the_query->have_posts() ) {
-        $the_query->the_post();
-        array_push($arrFinissants, $post);
-    }
-}
-wp_reset_postdata();
-?>
-                    <p class="ficheRealisation__infos__infoRealisation__nomAnnee__nom">Yves-Luc Bernier Bourdages</p>
-                    <p class="ficheRealisation__infos__infoRealisation__nomAnnee__annee">1re année</p>
+                    <p class="ficheRealisation__infos__infoRealisation__nomAnnee__nom">
+                        <?php echo get_field("prenom", $arrFinissants[0]->ID) ?> <?php echo get_field("nom", $arrFinissants[0]->ID) ?>
+                    </p>
+                    <p class="ficheRealisation__infos__infoRealisation__nomAnnee__annee"><?php echo $sessionAnnee?> année</p>
                 </div>
-                <p class="ficheRealisation__infos__infoRealisation__cours">Montage médiatique : plurimédia</p>
-                <p class="ficheRealisation__infos__infoRealisation__description">
-                    Notre mandat a été de concevoir une animation courte du logo d’un organisme réel de notre choix.
-                    Pour ce projet, j’ai décidé de prendre celui du cégep de Sainte-Foy et de présenter en quelques secondes
-                    toutes les différentes sphères d’activités auxquelles il participe.
-                </p>
+                <p class="ficheRealisation__infos__infoRealisation__cours"><?php echo get_field("nom", $arrCours[0]->ID) ?></p>
+                <div class="ficheRealisation__infos__infoRealisation__description"><?php echo get_field("description") ?></div>
                 <div class="ficheRealisation__infos__infoRealisation__technologies">
                     <p class="ficheRealisation__infos__infoRealisation__technologies__titreListe"><?php echo get_post(841)->titre?></p>
                     <div class="ficheRealisation__infos__infoRealisation__technologies__listes">
                         <ul class="ficheRealisation__infos__infoRealisation__technologies__liste">
-                            <li class="ficheRealisation__infos__infoRealisation__technologies__item">Adobe Illustrator</li>
-                            <li class="ficheRealisation__infos__infoRealisation__technologies__item">Adobe After Effects</li>
-                        </ul>
-                        <ul class="ficheRealisation__infos__infoRealisation__technologies__liste">
-                            <li class="ficheRealisation__infos__infoRealisation__technologies__item">Adobe Audition</li>
-                            <li class="ficheRealisation__infos__infoRealisation__technologies__item">Vimeo</li>
+                            <li class="ficheRealisation__infos__infoRealisation__technologies__item"><?php echo get_field("technologies") ?></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="ficheRealisation__infos__autresImages">
+            <?php
+            if(get_field("photo_2")["sizes"]["large"] != null) { ?>
                 <img
                         class="ficheRealisation__infos__autresImages__image"
-                        src="<?php echo get_field("photo_2")["sizes"]["large"]; ?>"
+                        src="<?php echo get_field("photo_2")["sizes"]["large"] ?>"
                         alt="image d'une réalisation"
                 >
+            <?php }
+            if(get_field("photo_3")["sizes"]["large"] != null) { ?>
                 <img
                         class="ficheRealisation__infos__autresImages__image"
-                        src="<?php echo get_field("photo_3")["sizes"]["large"]; ?>"
+                        src="<?php echo get_field("photo_3")["sizes"]["large"] ?>"
                         alt="image d'une réalisation"
                 >
+            <?php } ?>
             </div>
         </div>
         <div class="ficheRealisation__biographie">
             <div class="ficheRealisation__biographie__titreMedia">
-                <h2 class="ficheRealisation__biographie__titreMedia__titre">Yves-Luc Bernier Bourdages</h2>
+                <h2 class="ficheRealisation__biographie__titreMedia__titre"><?php echo get_field("prenom", $arrFinissants[0]->ID) ?> <?php echo get_field("nom", $arrFinissants[0]->ID) ?></h2>
                 <div class="ficheRealisation__biographie__titreMedia__media">
-                    <a href="<?php echo get_post(126)->texte?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-at"></a>
-                    <a href="<?php echo get_post(127)->texte?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-twitter"></a>
-                    <a href="<?php echo get_post(128)->texte?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-linkedin"></a>
-                    <a href="<?php echo get_post(148)->texte?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-globe"></a>
+                    <a href="mailto:<?php echo get_field("courriel", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-at"></a>
+<?php
+if(get_field("pseudo_twitter", $arrFinissants[0]->ID) != null){?>
+    <a href="https://twitter.com/<?php echo get_field("pseudo_twitter", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-twitter"></a>
+<?php }
+?>
+                    <a href="<?php echo get_field("linkedin", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-linkedin"></a>
+                    <a href="<?php echo get_field("site_web", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__titreMedia__media__lien fa fa-globe"></a>
                 </div>
             </div>
             <div class="ficheRealisation__biographie__imageTexte">
                 <img
                         class="ficheRealisation__biographie__imageTexte__image"
-                        src="https://via.placeholder.com/235x235"
-                        alt="image d'une réalisation"
+                        src="<?php echo get_field("photo", $arrFinissants[0]->ID)["sizes"]["large"]  ?>"
+                        alt="portrait de l'étudiant"
                 >
-                <p class="ficheRealisation__biographie__imageTexte__texte">
-                    La troisième fois c’est la bonne!
-                    N’ayant pas encore trouvé ma voie après avoir complété mes sciences de la nature et mon
-                    baccalauréat en comptabilité, j’ai entrepris leprogramme des TIM déterminée à trouver un domaine
-                    que j’allais vraiment aimer. Le dicton a eu bienraison. Après deux programmes qui n’ont pas allumé
-                    d’étincelle, j’ai enfin trouvé ma vraie passion dans le développement web.
-                    Dans ce domaine varié, je m’épanouis plus particulièrement lorsque je fais du design UX et UI et du
-                    développement front-end. Aujourd’hui, j’aspire à trouver un emploi dans lequel je vais pouvoir mettre
-                    à profit ma créativité et mon amour pour la résolution de problèmes.
-                </p>
+                <div class="ficheRealisation__biographie__imageTexte__texte">
+                    <?php echo get_field("presentation", $arrFinissants[0]->ID) ?>
+                </div>
             </div>
         </div>
         <div class="ficheRealisation__biographie__mobile ">
-            <h2 class="ficheRealisation__biographie__mobile__titre">Yves-Luc Bernier Bourdages</h2>
-            <p class="ficheRealisation__biographie__mobile__texte">
+            <h2 class="ficheRealisation__biographie__mobile__titre"><?php echo get_field("prenom", $arrFinissants[0]->ID) ?> <?php echo get_field("nom", $arrFinissants[0]->ID) ?></h2>
+            <div class="ficheRealisation__biographie__mobile__texte">
                 <img
                         class="ficheRealisation__biographie__mobile__image"
-                        src="https://via.placeholder.com/168x168"
+                        src="<?php echo get_field("photo", $arrFinissants[0]->ID)["sizes"]["large"]  ?>"
                         alt="image d'une réalisation"
                 >
-                La troisième fois c’est la bonne!
-                N’ayant pas encore trouvé ma voie après avoir complété mes sciences de la nature et mon
-                baccalauréat en comptabilité, j’ai entrepris le programme des TIM déterminée à trouver un domaine
-                que j’allais vraiment aimer. Le dicton a eu bienraison. Après deux programmes qui n’ont pas allumé
-                d’étincelle, j’ai enfin trouvé ma vraie passion dans le développement web.
-                Dans ce domaine varié, je m’épanouis plus particulièrement lorsque je fais du design UX et UI et du
-                développement front-end. Aujourd’hui, j’aspire à trouver un emploi dans lequel je vais pouvoir mettre
-                à profit ma créativité et mon amour pour la résolution de problèmes.
-            </p>
+                <?php echo get_field("presentation", $arrFinissants[0]->ID) ?>
+            </div>
             <div class="ficheRealisation__biographie__mobile__media">
-                <a href="<?php echo get_post(126)->texte?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-at"></a>
-                <a href="<?php echo get_post(127)->texte?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-twitter"></a>
-                <a href="<?php echo get_post(128)->texte?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-linkedin"></a>
-                <a href="<?php echo get_post(148)->texte?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-globe"></a>
+                <a href="mailto:<?php echo get_field("courriel", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-at"></a>
+<?php
+if (get_field("pseudo_twitter", $arrFinissants[0]->ID) != null){?>
+                <a href="https://twitter.com/<?php echo get_field("pseudo_twitter", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-twitter"></a>
+<?php }
+?>              <a href="<?php echo get_field("linkedin", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-linkedin"></a>
+                <a href="<?php echo get_field("site_web", $arrFinissants[0]->ID) ?>" class="ficheRealisation__biographie__mobile__media__lien fa fa-globe"></a>
             </div>
         </div>
         <div class="ficheRealisation__autresProjets">
-            <h2 class="ficheRealisation__autresProjets__titre"><?php echo get_post(841)->texte?></h2>
+            <h2 class="ficheRealisation__autresProjets__titre"><?php echo get_post(841)->texte?> <?php echo get_field("prenom", $arrFinissants[0]->ID) ?> <?php echo get_field("nom", $arrFinissants[0]->ID) ?></h2>
             <div class="ficheRealisation__autresProjets__flex">
-                <div class="ficheRealisation__autresProjets__galerie">
-                    <a class="ficheRealisation__autresProjets__galerie__realisation" href="#">
-                        <img
-                                class="ficheRealisation__autresProjets__galerie__realisation__image"
-                                src="https://via.placeholder.com/355x200"
-                                alt="image d'une réalisation"
-                        >
-                        <p class="ficheRealisation__autresProjets__galerie__realisation__titre">Festival OFF</p>
-                        <p class="ficheRealisation__autresProjets__galerie__realisation__nom">Yves-Luc Bernier Bourdages</p>
-                    </a>
-                </div>
-                <div class="ficheRealisation__autresProjets__galerie">
-                    <a class="ficheRealisation__autresProjets__galerie__realisation" href="#">
-                        <img
-                                class="ficheRealisation__autresProjets__galerie__realisation__image"
-                                src="https://via.placeholder.com/355x200"
-                                alt="image d'une réalisation"
-                        >
-                        <p class="ficheRealisation__autresProjets__galerie__realisation__titre">Festival OFF</p>
-                        <p class="ficheRealisation__autresProjets__galerie__realisation__nom">Yves-Luc Bernier Bourdages</p>
-                    </a>
-                </div>
+            <?php
+            //Les Projets
+            //**************************************************
+
+            //Requête pour obtenir les infos des finissants
+            $args = array(
+                'post_type' => 'projets',
+                'posts_per_page' => -1,
+                'post_status' => 'publish',
+                "meta_key" => "diplome_id",
+                "meta_value" => get_field("diplome_id"),
+            );
+
+            $the_query = new WP_Query($args);
+
+            if($the_query->have_posts()){
+                while ($the_query->have_posts()){
+                    $the_query->the_post();
+                    if($post->ID != $idProjetVue) { ?>
+                        <div class="ficheRealisation__autresProjets__galerie">
+                            <a class="ficheRealisation__autresProjets__galerie__realisation" href="<?php the_permalink();?>">
+                                <img
+                                        class="ficheRealisation__autresProjets__galerie__realisation__image"
+                                        src="<?php echo get_field("photo_1")["sizes"]["large"]; ?>"
+                                        alt="image d'une réalisation"
+                                >
+                                <p class="ficheRealisation__autresProjets__galerie__realisation__titre"><?php echo get_field("titre") ?></p>
+                                <p class="ficheRealisation__autresProjets__galerie__realisation__nom"><?php echo get_field("technologies") ?></p>
+                            </a>
+                        </div>
+                    <?php }
+                }
+            } ?>
             </div>
         </div>
-        <a class="ficheRealisation__boutonRetour boutonBleu" href="#"><i class="fa fa-arrow-left"></i><?php echo get_post(842)->titre?></a>
+        <a class="ficheRealisation__boutonRetour boutonBleu" href="<?php echo get_permalink(388)?>"><i class="fa fa-arrow-left"></i><?php echo get_post(842)->titre?></a>
         <div class="boutonHautPage">
             <a class="boutonHautPage__bouton fa fa-arrow-up" href="#top"></a>
         </div>
     </div>
 </main>
 
-<?php get_footer();?>      
+<?php get_footer();?>
