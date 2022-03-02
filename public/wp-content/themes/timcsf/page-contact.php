cb8e5de356cadfb64e228c75bc05ad417f71ff16
Bernier-Bourdages Yves-Luc <1961716@csfoy.ca>
2022-03-02 17:23:06 -0500

    commit mercredi 17h

--- public/wp-content/themes/timcsf/page-contact.php
+++ public/wp-content/themes/timcsf/page-contact.php
@@ -48,51 +48,51 @@
                 <?php }
             } ?>
         </div>
-        <form class="contact__formulaire">
-            <div class="contact__formulaire__groupInput">
+        <form class="contact__formulaire" id="form" method="POST" novalidate>
+            <div class="contact__formulaire__groupInput" id="divPrenom">
                 <label for="prenom" class="contact__formulaire__label">Prénom</label>
                 <input name="prenom" id="prenom" type="text" class="contact__formulaire__input" required>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
-                    <p class="contact__formulaire__erreur__p">Entrez votre prénom complet</p>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
+                    <p class="contact__formulaire__erreur__p"></p>
                 </div>
             </div>
             <div class="contact__formulaire__groupInput">
                 <label for="nom" class="contact__formulaire__label">Nom</label>
                 <input name="nom" id="nom" type="text" class="contact__formulaire__input" required>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
-                    <p class="contact__formulaire__erreur__p">Entrez votre nom complet</p>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
+                    <p class="contact__formulaire__erreur__p"></p>
                 </div>
             </div>
             <div class="contact__formulaire__groupInput">
                 <label for="courriel" class="contact__formulaire__label">Courriel</label>
                 <input name="courriel" id="courriel" type="email" class="contact__formulaire__input" required>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
                     <p class="contact__formulaire__erreur__p">Entrez un adresse courriel valide.</p>
                 </div>
             </div>
             <div class="contact__formulaire__groupInput">
                 <label for="destinataire" class="contact__formulaire__label">Destinataire</label>
                 <select name="destinataire" id="destinataire" class="contact__formulaire__input" required>
-                    <option disabled selected>Choisir un destinataire</option>
+                    <option value="" disabled selected>Choisir un destinataire</option>
                     <option value="<?php echo get_post(411)->prenom?>"><?php echo get_post(411)->prenom?> <?php echo get_post(411)->nom?>(<?php echo get_post(411)->responsabilite?>)</option>
                     <option value="<?php echo get_post(412)->prenom?>"><?php echo get_post(412)->prenom?> <?php echo get_post(412)->nom?>(<?php echo get_post(412)->responsabilite?>)</option>
                     <option value="<?php echo get_post(413)->prenom?>"><?php echo get_post(413)->prenom?> <?php echo get_post(413)->nom?>(<?php echo get_post(413)->responsabilite?>)</option>
                     <option value="<?php echo get_post(414)->prenom?>"><?php echo get_post(414)->prenom?> <?php echo get_post(414)->nom?>(<?php echo get_post(414)->responsabilite?>)</option>
                 </select>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
                     <p class="contact__formulaire__erreur__p">Sélectionnez un destinataire.</p>
                 </div>
             </div>
-            <div class="contact__formulaire__groupInput">
+            <div class="contact__formulaire__groupInput telephone">
                 <label for="telephone" class="contact__formulaire__label">Téléphone</label>
                 <p class="contact__formulaire__exemple">Ex:123-456-7890</p>
-                <input name="telephone" id="telephone" type="tel" class="contact__formulaire__input" pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$" required>
+                <input name="telephone" id="telephone" type="tel" class="contact__formulaire__input" required>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
                     <p class="contact__formulaire__erreur__p">Entrez votre numéro de téléphone au format 123-456-7890.</p>
                 </div>
                 <div class="contact__formulaire__autorisation">
@@ -104,7 +104,7 @@
                 <label for="sujet" class="contact__formulaire__label">Sujet</label>
                 <input name="sujet" id="sujet" type="text" class="contact__formulaire__input" required>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
                     <p class="contact__formulaire__erreur__p">Entrez le sujet du courriel.</p>
                 </div>
             </div>
@@ -112,7 +112,7 @@
                 <label for="message" class="contact__formulaire__label">Message</label>
                 <textarea name="message" id="message" class="contact__formulaire__input__message" required></textarea>
                 <div class="contact__formulaire__erreur">
-                    <i class="contact__formulaire__erreur__icon">icon Warning</i>
+                    <i class="contact__formulaire__erreur__icon fa fa-exclamation-circle"></i>
                     <p class="contact__formulaire__erreur__p">Votre message est absent.</p>
                 </div>
             </div>
@@ -124,5 +124,4 @@
         </div>
     </div>
 </main>
-
 <?php get_footer(); ?>
